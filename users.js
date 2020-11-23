32270edcd86768b5fb014142b1b08d4e99dba87e
biFebriansyah <bukanebi@gmail.com>
2020-11-23 13:51:29 +0700

    add users list

---
+++ users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        id: 01,
+        username: "admin",
+        role: "admin",
+    },
+    {
+        id: 02,
+        username: "tomy",
+        role: "admin",
+    },
+]
